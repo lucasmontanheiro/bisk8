54465c62b50bd8de583e0d6de5d57594ce73e981
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-25 09:47:34 -0800

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -5,6 +5,7 @@ echo <<<HTML
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <title>BISK8</title>
     <style>
         html, body {
