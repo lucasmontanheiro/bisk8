6eb98f958f400c8bf6708e9da7254e40bf02b0c2
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-27 14:54:48 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,37 +2,8 @@
 
 $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Define routes with additional configurations
-$pageRoutes = [
-    "/" => [
-        "iframe" => "2PACX-1vQsd1IPhwS4hNttet31WZUlzMY_J4QTEF5Flwb-t4j5X_fvgs62657ha9MAmgTxfkPS_bZwscz8NbF5",
-        "background_color" => "#ebe0cc",
-        "color" => "#252527",
-        "a_color" => "#0690d4",
-        "a_background_color" => "#f9da1d",
-    ],
-    "/bisk8" => [
-        "iframe" => "2PACX-1vRr5Bq510tPsxJ8yVnURqC7iWhrhEU7oR4l9PKWH_1xwNrdjscGFVWkI0pUNLWT1EIwgMw5nUQuAJQx",
-        "background_color" => "#9a9865",
-        "color" => "white",
-        "a_color" => "yellow",
-        "a_background_color" => "",
-    ],
-    "/geco" => [
-        "iframe" => "2PACX-1vTR0dB5PIY0M1clFqF-9O_oVZAVEx5TczGXpDiNddunEOfzGF_YpBz_NvTJ5BbLLH1vgLZ_X45hiDB5",
-        "background_color" => "#ebe0cc",
-        "color" => "#252527",
-        "a_color" => "#0690d4",
-        "a_background_color" => "#f9da1d",
-    ],
-    "/franguinho" => [
-        "iframe" => "2PACX-1vTDAcNLB7WbUYpkKYTM2gf9EXGjLvs_oVBJ876xWY2prsGz1ojnpYcQwXBp15G0uyB01YwzJ-b4uVbz",
-        "background_color" => "#ebe0cc",
-        "color" => "#252527",
-        "a_color" => "#0690d4",
-        "a_background_color" => "#f9da1d",
-    ]
-];
+// Load dynamic routes from a separate file
+$pageRoutes = include 'routes.php';
 
 // Check if route exists in static or dynamic routes
 if (array_key_exists($requestPath, $pageRoutes)) {
---
+++ routes.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    "/" => [
+        "iframe" => "2PACX-1vQsd1IPhwS4hNttet31WZUlzMY_J4QTEF5Flwb-t4j5X_fvgs62657ha9MAmgTxfkPS_bZwscz8NbF5",
+        "background_color" => "#ebe0cc",
+        "color" => "#252527",
+        "a_color" => "#0690d4",
+        "a_background_color" => "#f9da1d",
+    ],
+    "/bisk8" => [
+        "iframe" => "2PACX-1vRr5Bq510tPsxJ8yVnURqC7iWhrhEU7oR4l9PKWH_1xwNrdjscGFVWkI0pUNLWT1EIwgMw5nUQuAJQx",
+        "background_color" => "#9a9865",
+        "color" => "white",
+        "a_color" => "yellow",
+        "a_background_color" => "",
+    ],
+    "/geco" => [
+        "iframe" => "2PACX-1vTR0dB5PIY0M1clFqF-9O_oVZAVEx5TczGXpDiNddunEOfzGF_YpBz_NvTJ5BbLLH1vgLZ_X45hiDB5",
+        "background_color" => "#ebe0cc",
+        "color" => "#252527",
+        "a_color" => "#0690d4",
+        "a_background_color" => "#f9da1d",
+    ],
+    "/franguinho" => [
+        "iframe" => "2PACX-1vTDAcNLB7WbUYpkKYTM2gf9EXGjLvs_oVBJ876xWY2prsGz1ojnpYcQwXBp15G0uyB01YwzJ-b4uVbz",
+        "background_color" => "#ebe0cc",
+        "color" => "#252527",
+        "a_color" => "#0690d4",
+        "a_background_color" => "#f9da1d",
+    ]
+];
+?>
