640db984fa90489a47a7cdc79adce2b08d152538
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-27 14:29:03 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if (array_key_exists($requestPath, $pageRoutes)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{$title}</title>
-    <link rel="stylesheet" href="/assets/styles.css">
+    <link rel="stylesheet" href="/assets/css.css">
 
     <style>
         html, body {
