e35b2a4ea67e173b581a980eb853884060def90c
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-27 13:51:46 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
+// Define routes with corresponding configurations
+$pageRoutes = [
+    "/" => [
+        "title" => "BISK8 NOVA PAGINA",
+        "iframe" => "https://example.com/corinthians",
+        "background_color" => "#ebe0cc",
+        "color" => "#252527",
+        "a_color" => "#0690d4",
+        "a_background_color" => "#f9da1d",
+    ]
+];
+
+// Parse current request path
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Find the matching route or default to 404
+if (array_key_exists($requestPath, $pageRoutes)) {
+    $pageData = $pageRoutes[$requestPath];
+    $pageTitle = $pageData['title'];
+    $iframeSrc = $pageData['iframe'];
+    $cssFile = $pageData['css'];
+    include "../dir/main-template.php";
+} else {
+    http_response_code(404);
+    include "../templates/404.php"; // Optional 404 page
+    exit;
+}
+
+?>
+
+<!--
 
 $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $routes = [
@@ -6,6 +37,7 @@ $routes = [
     '/bisk8' => 'dir/bisk8.php',
     '/corinthians' => 'dir/corinthians.php',
     '/geco' => 'dir/geco.php',
+    '/franguinho' => 'dir/franguinho.php',
 ];
 
 if (array_key_exists($requestPath, $routes)) {
