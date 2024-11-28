837034f6d571a256f481a82c9980e51a699c0046
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-28 15:45:21 -0800

    Add files via upload

--- fetch-data.php
+++ fetch-data.php
@@ -21,9 +21,9 @@ function fetchSpreadsheetData($spreadsheetId, $range, $apiKey) {
 }
 
 // Google Sheets Config
-$spreadsheetId = "YOUR_SPREADSHEET_ID"; // Replace with your Google Sheet ID
-$range = "Sheet1!A1:E"; // Adjust the range based on your spreadsheet
-$apiKey = "YOUR_API_KEY"; // Replace with your API key
+$spreadsheetId = "1tXFNjLHGGlmgqKi50oSFUYlqZ_raTNDJPYpSMx0yzx0"; // Replace with your Google Sheet ID
+$range = "Sheet1!A5:V5423"; // Adjust the range based on your spreadsheet
+$apiKey = "AIzaSyDEp6eo3vt0Y-iQOC6mj8V-j1sXM4io7YU"; // Replace with your API key
 
 // Fetch spreadsheet data
 $data = fetchSpreadsheetData($spreadsheetId, $range, $apiKey);
