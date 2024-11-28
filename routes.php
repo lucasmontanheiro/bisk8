ccb2028973eabd370deb34035cf4b95bd98ec32e
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-11-28 15:02:07 -0800

    Add files via upload

--- routes.php
+++ routes.php
@@ -44,6 +44,16 @@ return [
         "color" => "#252527",
         "a_color" => "#0690d4",
         "a_background_color" => "#f9da1d",
+    ],
+    ,
+    "/timeline" => [
+        "template" => "template_timeline",
+        "title" => "timeline",
+        "iframe" => "2PACX-1vTDAcNLB7WbUYpkKYTM2gf9EXGjLvs_oVBJ876xWY2prsGz1ojnpYcQwXBp15G0uyB01YwzJ-b4uVbz",
+        "background_color" => "#ebe0cc",
+        "color" => "#252527",
+        "a_color" => "#0690d4",
+        "a_background_color" => "#f9da1d",
     ]
 ];
 ?>
