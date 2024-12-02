2089c8123129f43de80d01f41d691ce3f8fd0a8c
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2024-12-02 10:27:00 -0800

    Add files via upload

--- routes.php
+++ routes.php
@@ -15,7 +15,7 @@ return [
         "iframe" => "2PACX-1vSc3DRhpFLS6BYX5byaM8SS89f36b_MW5U0jkAjw79KmqY76O4YAXNusvF1FWsBcJ59RHf_oXJiBDLa",
         "background_color" => "#fff3cc",
         "color" => "#212529",
-        "a_color" => "yellow",
+        "a_color" => "red",
         "a_background_color" => "",
     ],
     "/bisk8_1" => [
