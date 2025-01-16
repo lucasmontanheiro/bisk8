e834d362ad2c2af3e751a1a76be6d6140f3dcc20
lucasmontanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2025-01-16 13:18:30 -0800

    Add files via upload

--- template_home.php
+++ template_home.php
@@ -16,27 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="/static/css.css">
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            height: 350px;
-            width: 100%;
-            background-color: <?php echo $backgroundColor; ?>;
-            color: <?php echo $color; ?>;
-        }
-        iframe {
-            border: none;
-            width: 100%;
-            height: 100%;
-        }
-        a {
-            color: <?php echo $aColor; ?>;
-            background-color: <?php echo $aBackgroundColor; ?>;
-            text-decoration: none;
-            border-radius: 5px;
-        }
-    </style>
 </head>
 <body>
 
