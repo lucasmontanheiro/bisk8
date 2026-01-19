a5fcd9239b0b21faa01aa90a53dd17fe0e5a5e6a
Lucas Montanheiro <66181013+lucasmontanheiro@users.noreply.github.com>
2026-01-19 13:37:20 -0300

    Delete archive.php

--- archive.php
+++
@@ -1,1 +0,0 @@
-<iframe src="https://docs.google.com/document/d/e/2PACX-1vRr5Bq510tPsxJ8yVnURqC7iWhrhEU7oR4l9PKWH_1xwNrdjscGFVWkI0pUNLWT1EIwgMw5nUQuAJQx/pub?embedded=true" width="100%" height="100%"></iframe>
